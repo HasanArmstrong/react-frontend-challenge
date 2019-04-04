aa7733d2ed62fb0d0a76f96849c5eefb3529a8ef
Hasan Armstrong <hasan_armstrong@hotmail.co.uk>
2019-04-04 23:47:07 +0700

    clean code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from './Form';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form";
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state ={
-			output: ''
-		}
-	}
+  constructor() {
+    super();
+    this.state = {
+      output: ""
+    };
+  }
 
-	handleChange = (e) => {
-	this.setState({
-		[e.target.name]: e.target.value.split("").reverse()
-	}), () => console.log(this.state)
-	}
-	render() {
-		return (
-			<div className="App">
-				<h1>INPUT</h1>
-				<input  onChange={this.handleChange} placeholder="Write something here" name="output"/>
-				<h2>OUTPUT</h2>
-				<h1>{this.state.output}</h1>
-			</div>
-		);
-	}
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value.split("").reverse()
+    }),
+      () => console.log(this.state);
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>INPUT</h1>
+        <input
+          onChange={this.handleChange}
+          placeholder="Write something here"
+          name="output"
+        />
+        <h2>OUTPUT</h2>
+        <h1>{this.state.output}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
